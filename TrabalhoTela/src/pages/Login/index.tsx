ee9ccd3859efefcd57bb8e846ce49f8ac7219e5f
scarvalh <58118431+scarvalh@users.noreply.github.com>
2020-12-14 16:32:16 -0300

    Add files via upload

--- TrabalhoTela/src/pages/Login/index.tsx
+++ TrabalhoTela/src/pages/Login/index.tsx
@@ -1,15 +1,28 @@
-//import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
-import TopBar from '../../components/TopBar';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 
 export default function Login() {
-  //const navigation = useNavigation();
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <TopBar />
+      <View>
+        <Text style={styles.textSt}>Entrar como:</Text>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('homeClient')}>
+              <Text style={styles.buttonText}>Cliente</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('homeCompany')}>
+              <Text style={styles.buttonText}>Empresa</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
--- TrabalhoTela/src/pages/Login/styles.tsx
+++ TrabalhoTela/src/pages/Login/styles.tsx
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#00B7F1',
     paddingTop: Constants.statusBarHeight + 20,
   },
   textSt: {
