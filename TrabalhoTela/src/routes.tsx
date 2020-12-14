ad7b762106facb7d277a9f1ebc6d07aec58cbb20
scarvalh <58118431+scarvalh@users.noreply.github.com>
2020-12-14 16:35:22 -0300

    Add files via upload

--- TrabalhoTela/src/routes.tsx
+++ TrabalhoTela/src/routes.tsx
@@ -1,35 +1,27 @@
-import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 import GeracaoCodigo from './pages/GeracaoCodigo';
+import homeClient from './pages/homeClient';
+import homeCompany from './pages/homeCompany';
 import Login from './pages/Login';
 import ResgatarCodigo from './pages/ResgatarCodigo';
 
-// const AppStack = createStackNavigator();
 
-// export default function routes() {
-//   return (
-//     <NavigationContainer>
-//       <AppStack.Navigator screenOptions={{
-//         headerShown: false
-//       }}>
-//         <AppStack.Screen name="Login" component={Login} />
-//         <AppStack.Screen name="GeracaoCodigo" component={GeracaoCodigo} />
-//         <AppStack.Screen name="ResgatarCodigo" component={ResgatarCodigo} />
-//       </AppStack.Navigator>
-//     </NavigationContainer>
-//   )
-// }
+const AppStack = createStackNavigator();
 
-const clientDrawerNavigator = createDrawerNavigator({
-  'Home': {
-    screen: Login
-  },
-  'Gerar código de pontos': {
-    screen: GeracaoCodigo
-  },
-  'Validar código': {
-    screen: ResgatarCodigo
-  }
-});
-
-export default createAppContainer(clientDrawerNavigator);
+export default function routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <AppStack.Screen name="Login" component={Login} />
+        <AppStack.Screen name="GeracaoCodigo" component={GeracaoCodigo} />
+        <AppStack.Screen name="ResgatarCodigo" component={ResgatarCodigo} />
+        <AppStack.Screen name="homeClient" component={homeClient} />
+        <AppStack.Screen name="homeCompany" component={homeCompany} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  )
+} 
