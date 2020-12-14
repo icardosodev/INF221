dd9dd6edd75594f2785c0c43684bda26a9bc9031
scarvalh <58118431+scarvalh@users.noreply.github.com>
2020-12-14 16:34:38 -0300

    Add files via upload

---
+++ TrabalhoTela/src/pages/homeCompany/index.tsx
@@ -0,0 +1,15 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View } from 'react-native';
+import TopBar from '../../components/TopBar';
+import styles from './styles';
+
+export default function homeCompany() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <TopBar />
+    </View>
+  )
+}
